28073a840a95a3d0b6905ad8f167c6b76fc432a7
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-20 16:06:09 -0400

    updated imports in index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // code away!
 const express = require('express');
-const initMiddleware = require('./src/middleware');
+const initMiddleware = require('./src/serverMiddleware');
 const router = require('./src/router');
 
 const app = express();
 initMiddleware(app);
 app.use('/api', router);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6500;
 
 app.listen(PORT, (err) => {
     if(err) throw err;
